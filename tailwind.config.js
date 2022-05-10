6c1c5842ed336c7dd867f8c4bed36eff6d97ed26
Al-Imran <alimran21feb@gmail.com>
2022-05-10 22:51:03 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctorstheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#E4E5E9",
+        },
+      },
+
+    ],
+  },
+  plugins: [require("daisyui")],
+}
