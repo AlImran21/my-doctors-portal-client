d0a0d3adb5adebde2e5689625f15e6ea6d74b6ba
Al-Imran <alimran21feb@gmail.com>
2022-05-18 18:37:41 +0600

    added live site link

---
+++ README.md
@@ -0,0 +1,3 @@
+# Doctors Portal
+
+#### Click here to visit live site link: [https://doctors-portal-b0153.web.app/](https://doctors-portal-b0153.web.app/)
--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -5,7 +5,7 @@ const UserRow = ({ user, refetch }) => {
     const { email, role } = user;
 
     const makeAdmin = () => {
-        fetch(`http://localhost:5000/user/admin/${email}`, {
+        fetch(`https://gentle-retreat-95722.herokuapp.com/user/admin/${email}`, {
             method: 'PUT',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
